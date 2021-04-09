cf1a48c523e9c0f72012b32581a350cd699649d5
Giot <lemarcogiot@gmail.com>
2021-04-09 15:34:32 +0200

    codage du formulaire de renseignement en js

--- js/panier.js
+++ js/panier.js
@@ -29,3 +29,94 @@ document.getElementById("supp").addEventListener('click', (e)=>{
     localStorage.clear()
     document.location.reload()
 })
+//---------------------------------cration du formaulaire d'envoi--------------------------------------------
+const form =`
+<form action="/ma-page-de-traitement" method="post" onsubmit="return valide()">
+    <div>
+        <label for="nom">Nom</label>
+        <input name="nom" id="nom" placeholder="Nom" autocomplete="family-name">
+    </div>
+    <div>
+        <label for="prenom">Prénom</label>
+        <input id="prenom" name="prenom" placeholder="Prénom" autocomplete="given-name">
+    </div>
+    <div>
+        <label for="adresse">Adresse</label>
+        <input id="adresse" name="user_adresse" placeholder="Adresse" autocomplete="address-line1" >
+    </div>
+    <div>
+        <label for="cp">Code postal</label>
+        <input type="text" id="cp" name="user_cp" placeholder="Code postal" autocomplete="postal-code" pattern="[0-9][0-9][0-9][0-9][0-9]">
+    </div>
+    <div>
+        <label for="city">Ville</label>
+        <input type="text" id="city" name="user_city" placeholder="Ville" autocomplete="address-level2">
+    </div>
+    <div>
+        <label for="email">Email</label>
+        <input type="email" name="email" id="email" placeholder="Email" autocomplete="email">
+    </div>
+    <div>
+        <label for="comment">Commentaires</label>
+        <input type="text" id="comment" name="user_comment">
+    </div>
+    <div>
+        <input type="submit" id="submit" name="envoyer">
+    </div>
+</form>
+`
+document.getElementById("form").innerHTML=(form)
+
+
+
+
+//envoie du formulaire dans le local sorage quand on clique sur envoyer
+const btnEnv = document.getElementById("submit")
+btnEnv.addEventListener('click', (e) => {
+    e.preventDefault()
+    
+
+    function valide() {
+        if (produitLocalStorage && document.getElementById("nom").value !="" && document.getElementById("prenom").value !="" && document.getElementById("adresse").value !="" && document.getElementById("cp").value !="" && document.getElementById("city").value !="" &&document.getElementById("email").value !=""){
+            const formFull = {
+                nom: document.getElementById("nom").value,
+                prenom: document.getElementById("prenom").value,
+                adresse: document.getElementById("adresse").value,
+                cp: document.getElementById("cp").value,
+                city: document.getElementById("city").value,
+                email: document.getElementById("email").value,
+                comment: document.getElementById("comment").value
+            }
+        
+            localStorage.setItem("form", JSON.stringify(formFull))
+        }
+        else if(!produitLocalStorage){
+            alert('Pannier vide')
+            
+        }
+        else{
+            alert("Saisissez toute les information")
+        }
+    }
+    valide()
+
+
+    //creation de l'array pour les information client
+    const formFull = {
+        nom: document.getElementById("nom").value,
+        prenom: document.getElementById("prenom").value,
+        adresse: document.getElementById("adresse").value,
+        cp: document.getElementById("cp").value,
+        city: document.getElementById("city").value,
+        email: document.getElementById("email").value,
+        comment: document.getElementById("comment").value
+    }
+
+    localStorage.setItem("form", JSON.stringify(formFull))
+
+    const envoieServ={
+        produitLocalStorage,
+        formFull
+    }
+})
+
